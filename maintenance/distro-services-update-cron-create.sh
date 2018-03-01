8e9273f08d91d17358dbe1134f442080c4aff5ce
Tom <tom.eley@gmail.com>
2018-03-01 16:23:31 +0000

    Minor change to make sure variables are reset

--- maintenance/distro-services-update-cron-create.sh
+++ maintenance/distro-services-update-cron-create.sh
@@ -8,13 +8,13 @@ echo "Are you sure you want to create a cron job to run every night at 12am to u
 source "$SCRIPTPATH/inc/pause.sh"
 
 echo -e "$YELLOW--->Creating Update Packages and Services Cron Job...$ENDCOLOR"
-croncmd="$SCRIPTPATH/maintenance/distro-services-update-cron.sh #AtoMiC-ToolKit"
-cronjob="0 0 * * * $croncmd"
+CRONCMD="$SCRIPTPATH/maintenance/distro-services-update-cron.sh #AtoMiC-ToolKit"
+CRONJOB="0 0 * * * $CRONCMD"
 
-if crontab -l | grep -q "$croncmd" ; then
+if crontab -l | grep -q "$CRONCMD" ; then
     echo 'Cron job already exists'
 else
-    if ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab - ; then
+    if ( crontab -l | grep -v -F "$CRONCMD" ; echo "$CRONJOB" ) | crontab - ; then
         echo "Cron job created, This will pick up any future service installs as well."
         echo "To easily adjust the time/frequency, install webmin and find it under the scheduled cron jobs menu option."
     else
--- maintenance/distro-services-update-cron-remove.sh
+++ maintenance/distro-services-update-cron-remove.sh
@@ -8,10 +8,10 @@ echo "Are you sure you want to remove a cron job?"
 source "$SCRIPTPATH/inc/pause.sh"
 
 echo -e "$YELLOW--->Creating Update Packages and Services Cron Job...$ENDCOLOR"
-croncmd="$SCRIPTPATH/maintenance/distro-services-update-cron.sh #AtoMiC-ToolKit"
+CRONCMD="$SCRIPTPATH/maintenance/distro-services-update-cron.sh #AtoMiC-ToolKit"
 
-if crontab -l | grep -q "$croncmd" ; then
-    if ( crontab -l | grep -v -F "$croncmd" ) | crontab - ; then
+if crontab -l | grep -q "$CRONCMD" ; then
+    if ( crontab -l | grep -v -F "$CRONCMD" ) | crontab - ; then
         echo "Cron job Removed"
     fi
 else
