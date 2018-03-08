4aafdc54d342d4c04eb2ccc7bd4c75e7c6ace860
Tom <tom.eley@gmail.com>
2018-03-08 19:32:43 +0000

    Changed question around adding subdomain name.

--- utils/duckdns/duckdns-questions.sh
+++ utils/duckdns/duckdns-questions.sh
@@ -3,7 +3,7 @@ echo "This will create a cron job to run every 5mins to keep your IP up to date 
 echo -e "${CYAN}Please make sure you have registered for an account at ${GREEN}https://www.duckdns.org$ENDCOLOR"
 source "$SCRIPTPATH/inc/pause.sh"
 
-DOMAINNAME=$(whiptail --inputbox "What is your Duck DNS Sub Domain names? (for more than one, use a comma separated list) i.e SomeSite" \
+DOMAINNAME=$(whiptail --inputbox "What is your Duck DNS Sub Domain names without .duckdns.org? i.e MySubSiteName (for more than one, use a comma separated list)" \
 10 60 --title "Domain Name" 3>&1 1>&2 2>&3)
 exitstatus=$?
 
