7b09fc8bb3a3dc6b7301c1cc2b24a80f72294047
Tom <tom.eley@gmail.com>
2018-03-14 12:37:43 +0000

    Changed Ombi from v2 v3

--- ombi/ombi-installer.sh
+++ ombi/ombi-installer.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # Script Name: AtoMiC Ombi Installer
 
-source "$SCRIPTPATH/inc/app-setup-check.sh"
 source "$SCRIPTPATH/inc/commons.sh"
 source "$SCRIPTPATH/inc/header.sh"
 echo -e "${GREEN}AtoMiC $APPTITLE Installer Script$ENDCOLOR"
 source "$SCRIPTPATH/inc/pause.sh"
 source "$SCRIPTPATH/inc/app-autostart-remove.sh"
 source "$SCRIPTPATH/inc/app-move-previous.sh"
-source "$SCRIPTPATH/utils/mono/mono-installer.sh"
-source "$SCRIPTPATH/ombi/ombi-constants.sh"
 source "$SCRIPTPATH/inc/app-install-deps.sh"
 source "$SCRIPTPATH/inc/app-folders-create.sh"
 source "$SCRIPTPATH/ombi/ombi-download.sh"
