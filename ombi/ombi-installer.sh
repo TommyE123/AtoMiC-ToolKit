c8c53661879eff2d48eec4a6e6d7ff5568da578a
Tom <tom.eley@gmail.com>
2018-03-15 21:49:27 +0000

    Remove old version of ombi

--- ombi/ombi-installer.sh
+++ ombi/ombi-installer.sh
@@ -6,6 +6,16 @@ source "$SCRIPTPATH/inc/header.sh"
 echo -e "${GREEN}AtoMiC $APPTITLE Installer Script$ENDCOLOR"
 source "$SCRIPTPATH/inc/pause.sh"
 source "$SCRIPTPATH/inc/app-autostart-remove.sh"
+
+# Added 15/03/2018 - Can be removed once users have updated.
+# Removes old install.
+if [[ -d /opt/ombi ]]; then
+    if sudo rm -r '/opt/ombi'; then
+        echo
+        echo 'Removed old /opt/ombi as its not compatable with Ombi V3'
+    fi
+fi
+
 source "$SCRIPTPATH/inc/app-move-previous.sh"
 source "$SCRIPTPATH/inc/app-install-deps.sh"
 source "$SCRIPTPATH/inc/app-folders-create.sh"
