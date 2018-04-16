3e7f637debacea454d86f258ea10e37ce03bbf19
Tom <tom.eley@gmail.com>
2018-04-16 19:05:05 +0100

    Added Aria2-WebUI

---
+++ aria2-webui/aria2-webui-constants.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+APPNAME='aria2-webui'
+APPUSESNGINX='YES'
+APPPATH='/var/www/aria2-webui'
+APPTITLE='Aria2-webui'
+APPGIT='https://github.com/ziahamza/webui-aria2.git'
+APPDPORT='aria2-webui'
+ACCESSHOST='NA'
---
+++ aria2/aria2-menu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Script Name: AtoMiC Aria2 Menu
+
+SUBCHOICE=$(whiptail --title "AtoMiC Toolkit - Manage Aria2" \
+--menu "What would you like to do?" --backtitle "$BACKTITLE" \
+--fb --cancel-button "Exit" $LINES $COLUMNS "$NETLINES" \
+"Install" "Install Aria2" \
+"Uninstall" "Uninstall Aria2" \
+"Manual Update" "Manually update Aria2" \
+"Secret Token" "View Secret token" \
+"Go Back" "Back to Main Menu" 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [[ $exitstatus = 0 ]]; then
+    source "$SCRIPTPATH/aria2/aria2-constants.sh"
+    case "$SUBCHOICE" in
+        "Install" ) source "$SCRIPTPATH/$APPNAME/$APPNAME-installer.sh" ;;
+        "Uninstall" ) source "$SCRIPTPATH/$APPNAME/$APPNAME-uninstaller.sh" ;;
+        "Manual Update" ) source "$SCRIPTPATH/$APPNAME/$APPNAME-update.sh" ;;
+        "Secret Token" ) source "$SCRIPTPATH/$APPNAME/$APPNAME-secret-token-controller.sh" ;;
+        "Go Back" ) source "$SCRIPTPATH/menus/menu-download-managers.sh" ;;
+        *) source "$SCRIPTPATH/inc/invalid-option.sh" ;;
+    esac
+    source "$SCRIPTPATH/inc/thankyou.sh"
+    source "$SCRIPTPATH/inc/exit.sh"
+else
+    source "$SCRIPTPATH/inc/thankyou.sh"
+    echo
+    sleep 1
+    exit 0
+fi
