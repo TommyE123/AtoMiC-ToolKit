b557692de69d842ca1182a48574c85f977785e4e
Tom <tom.eley@gmail.com>
2018-04-18 19:23:49 +0100

    Glances improvements

--- glances/glances-uninstaller.sh
+++ glances/glances-uninstaller.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Script Name: AtoMiC netdata Uninstaller
 
-source "$SCRIPTPATH/inc/commons.sh"
 source "$SCRIPTPATH/inc/header.sh"
 echo -e "${GREEN}AtoMiC $APPTITLE Uninstaller Script$ENDCOLOR"
 source "$SCRIPTPATH/inc/pause.sh"
 source "$SCRIPTPATH/inc/app-autostart-remove.sh"
 echo
 echo -e "$YELLOW--->Uninstalling pip $APPTITLE package...$ENDCOLOR"
-sudo -H pip uninstall -y $APPNAME
+sudo -H pip3 uninstall -y "$APPNAME"
 source "$SCRIPTPATH/inc/app-uninstall-confirmation.sh"
 source "$SCRIPTPATH/inc/thankyou.sh"
 source "$SCRIPTPATH/inc/exit.sh"
